fe9101fe9f73d53df31b73e5c2c7f0e9814c60b7
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-02-22 19:42:24 +0200

    Update lineage_blossom.mk

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -17,8 +17,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
